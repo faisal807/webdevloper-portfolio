6239dd7dfeee154070330a174c114a9ed7f5c16f
Faisal807 <lasiafdemha807@gmail.com>
2023-07-10 01:48:29 +0600

    a man commit

--- js/script.js
+++ js/script.js
@@ -1,16 +1,14 @@
-function sendEmail(){
-    sendEmail.send({
-        Host: "smtp.gmail.com",
-        Username :"webobd5@gmail.com",
-        Password: "@Iamcute",
-        To: 'fa78658@gmail.com',
-        From: document.getElementById("email").value,
-        Subject: "New Contact Form Enquiry",
-        Body: "Name:" + document.getElementById("name").value
-        +"<br> Email: " + document.getElementById("email").value
-        +"<br> Message: " + document.getElementById("message").value
-
-    }).then(function (message) {
-        alert("Message sent successfully")
-    });
-}
+function sendEmail() {
+    Email.send({
+      SecureToken:" a9f591dd-948e-4b9c-91a3-5fe9c8539bc1",
+      To: 'webobd5@gmail.com',
+      From: document.getElementById("email").value,
+      Subject: "Sending Email using javascript",
+      Body: "Name:" + document.getElementById("name").value
+      +"<br> Email: " + document.getElementById("email").value
+      +"<br> Message: " + document.getElementById("message").value
+    })
+      .then(function (message) {
+        alert("mail sent successfully")
+      });
+  }
